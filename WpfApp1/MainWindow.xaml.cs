853b68241f55c04a59fdf969c602b8c74b032c93
冬 余 <1016724958@qq.com>
2024-12-23 20:16:45 +0800

    添加GetSystemExecutionState

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -31,5 +31,49 @@ namespace WpfApp1
         {
             SystemSleeps.AllowSleep();
         }
+
+
+        private enum SystemInformationClass
+        {
+            SystemExecutionState = 16
+        };
+
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern uint CallNtPowerInformation(
+            SystemInformationClass information,
+            IntPtr inputBuffer,
+            uint inputBufferLength,
+            IntPtr outputBuffer,
+            uint outputBufferLength
+        );
+
+        private void GetDetailButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var powerState = GetSystemExecutionState();
+            MessageBox.Show(powerState.ToString());
+        }
+        /// <summary>
+        /// 获取系统执行状态
+        /// 0表示无，可正常睡眠、息屏
+        /// 1表示阻止睡眠中，系统无法睡眠
+        /// 2表示阻止息屏中，系统无法息屏
+        /// 3表示阻止睡眠以及息屏中，系统无法睡眠、息屏
+        /// </summary>
+        /// <returns>返回系统执行状态，如果查询失败，返回<see cref="EXECUTION_STATE.ES_CONTINUOUS"/></returns>
+        public static EXECUTION_STATE GetSystemExecutionState()
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(sizeof(uint));
+            if (CallNtPowerInformation(SystemInformationClass.SystemExecutionState,
+                    IntPtr.Zero, 0, ptr, sizeof(uint)) == 0)
+            {
+                var objectState = Marshal.PtrToStructure(ptr, typeof(uint));
+                var stringState = objectState?.ToString();
+                if (stringState != "0" && Enum.TryParse(stringState, out EXECUTION_STATE state))
+                {
+                    return state;
+                }
+            }
+            return EXECUTION_STATE.ES_CONTINUOUS;
+        }
     }
 }
--- WpfApp1/SystemSleeps.cs
+++ WpfApp1/SystemSleeps.cs
@@ -27,26 +27,25 @@ namespace WpfApp1
 
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
-        /// <summary>
-        /// 使应用程序能够通知系统它正在使用中，从而防止系统在应用程序运行时进入睡眠状态或关闭显示器。
-        /// 通知应用之后的状态
-        /// </summary>
-        [FlagsAttribute]
-        public enum EXECUTION_STATE : uint
-        {
-            /// <summary>
-            /// 通知系统正在设置的状态应保持有效，直到使用 ES_CONTINUOUS 的下一次调用和清除其他状态标志之一。
-            /// </summary>
-            ES_CONTINUOUS = 0x80000000,
-            /// <summary>
-            /// 防止显示器关闭
-            /// </summary>
-            ES_DISPLAY_REQUIRED = 0x00000002,
-            /// <summary>
-            /// 防止系统进入睡眠
-            /// </summary>
-            ES_SYSTEM_REQUIRED = 0x00000001
-        }
     }
-
+    /// <summary>
+    /// 使应用程序能够通知系统它正在使用中，从而防止系统在应用程序运行时进入睡眠状态或关闭显示器。
+    /// 通知应用之后的状态
+    /// </summary>
+    [FlagsAttribute]
+    public enum EXECUTION_STATE : uint
+    {
+        /// <summary>
+        /// 通知系统正在设置的状态应保持有效，直到使用 ES_CONTINUOUS 的下一次调用和清除其他状态标志之一。
+        /// </summary>
+        ES_CONTINUOUS = 0x80000000,
+        /// <summary>
+        /// 防止系统进入睡眠
+        /// </summary>
+        ES_SYSTEM_REQUIRED = 0x00000001,
+        /// <summary>
+        /// 防止显示器关闭
+        /// </summary>
+        ES_DISPLAY_REQUIRED = 0x00000002,
+    }
 }
